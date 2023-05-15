3ecd12b696457e1ed6a100da32f24042fe586c98
Circle-ma <a859576084@gmail.com>
2023-05-15 20:56:29 +0800

    finish fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import useGoodStore from "./Service/fetchData";
+
 function App() {
-    return <></>;
+    const { good, setGood } = useGoodStore();
+
+    useEffect(() => {
+        setGood();
+    }, []);
+
+    console.log(good);
 }
 
 export default App;
--- src/Service/fetchData.tsx
+++ src/Service/fetchData.tsx
@@ -1,94 +1,73 @@
 import { create } from "zustand";
 import Good from "../Model/good";
-import { useEffect } from "react";
 
-interface Store {
-    data: any;
-    fetchData: () => Promise<void>;
+interface GoodState {
+    good: Good | null;
+    setGood: () => void;
 }
 
-const useDataStore = create<Store>((set) => ({
-    data: null,
-    fetchData: async () => {
-        const response = await fetch(
-            "https://orderhkuat.pokeguide.com/api/v1/goods/2"
-        );
-        const data = await response.json();
-        set({ data: data });
+const useGoodStore = create<GoodState>((set) => ({
+    good: null,
+    setGood: () => {
+        fetch("https://orderhkuat.pokeguide.com/api/v1/goods/2")
+            .then((response) => response.json())
+            .then((data) => {
+                // Convert data to Good model
+                const goodData = {
+                    id: data.good.goods_id,
+                    name: data.good.goods_name,
+                    sku: data.good.goods_sku.map(
+                        (sku: {
+                            id: any;
+                            price: any;
+                            remaining_inventory: any;
+                            sku_images: { url: any }[];
+                            sku_option_mappings: any[];
+                        }) => ({
+                            id: sku.id,
+                            price: sku.price,
+                            remainingInventory: sku.remaining_inventory,
+                            image: sku.sku_images[0].url,
+                            skuOptionMappings: sku.sku_option_mappings.map(
+                                (mapping: {
+                                    option_id: any;
+                                    option_value_id: any;
+                                }) => ({
+                                    optionId: mapping.option_id,
+                                    optionValueId: mapping.option_value_id,
+                                })
+                            ),
+                        })
+                    ),
+                    maxPrice: data.good.max_price,
+                    minPrice: data.good.min_price,
+                    image: data.good.goods_images[0].url,
+                    options: data.good.options.map(
+                        (option: {
+                            option_id: any;
+                            option_name: any;
+                            option_values: any[];
+                        }) => ({
+                            optionId: option.option_id,
+                            optionName: option.option_name,
+                            optionValues: option.option_values.map(
+                                (value: {
+                                    option_value_id: any;
+                                    option_value_name: any;
+                                }) => ({
+                                    optionValueId: value.option_value_id,
+                                    optionValueName: value.option_value_name,
+                                })
+                            ),
+                        })
+                    ),
+                    is_sold_out: data.good.is_sold_out,
+                };
+                console.log(goodData);
+                set(() => ({ good: goodData }));
+            })
+            .catch((error) => console.error(error));
     },
 }));
 
-function transformData(): Good {
-    const { data, fetchData } = useDataStore();
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const good = data.good;
-
-    const options = good.options.map(
-        (option: {
-            option_id: any;
-            option_name: any;
-            option_values: any[];
-        }) => ({
-            optionId: option.option_id,
-            optionName: option.option_name,
-            optionValues: option.option_values.map(
-                (optionValue: {
-                    option_value_id: any;
-                    option_value_name: any;
-                }) => ({
-                    optionValueId: optionValue.option_value_id,
-                    optionValueName: optionValue.option_value_name,
-                })
-            ),
-        })
-    );
-
-    const sku = good.goods_sku.map(
-        (sku: {
-            id: any;
-            price: any;
-            remaining_inventory: any;
-            sku_images: string | any[];
-            price_options: any[];
-        }) => ({
-            id: sku.id,
-            price: sku.price,
-            remainingInventory: sku.remaining_inventory,
-            image:
-                sku.sku_images.length > 0
-                    ? sku.sku_images[0].url
-                    : good.goods_images[0].url,
-            skuOptionMappings: sku.price_options.map(
-                (priceOption: { id: any }) => ({
-                    optionId:
-                        good.options.find((option: { option_values: any[] }) =>
-                            option.option_values.some(
-                                (optionValue: { option_value_id: any }) =>
-                                    optionValue.option_value_id ===
-                                    priceOption.id
-                            )
-                        )?.option_id ?? -1,
-                    optionValueId: priceOption.id,
-                })
-            ),
-        })
-    );
-
-    return {
-        id: good.goods_id,
-        name: good.goods_name,
-        sku,
-        maxPrice: good.max_price,
-        minPrice: good.min_price,
-        image:
-            good.goods_images.length > 0 ? good.goods_images[0].url : undefined,
-        options,
-        is_sold_out: good.is_sold_out,
-    };
-}
-
-export default transformData;
+export default useGoodStore;
